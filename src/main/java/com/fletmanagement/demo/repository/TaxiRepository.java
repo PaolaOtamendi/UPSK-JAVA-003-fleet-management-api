ea518f75cdfb60a47e7aff0f125a0e61c29c7a05
PaolaOtamendi <paola_otamendi23@hotmail.com>
2024-03-01 16:46:58 -0600

    fix servicicios de taxi y paginacion

--- src/main/java/com/fletmanagement/demo/repository/TaxiRepository.java
+++ src/main/java/com/fletmanagement/demo/repository/TaxiRepository.java
@@ -1,9 +1,11 @@
 package com.fletmanagement.demo.repository;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.fletmanagement.demo.model.Taxi;
 
 @Repository
 public interface TaxiRepository extends JpaRepository<Taxi, Integer> {
+    Page<Taxi> findAll(Pageable pageable);
 }
