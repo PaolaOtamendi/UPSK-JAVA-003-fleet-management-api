2586a6105fe1d6aa7f9df47c203bae239d349bb4
PaolaOtamendi <paola_otamendi23@hotmail.com>
2024-03-19 15:40:07 -0600

    fix query repositorio de ultima ubicacionss de taxis

--- src/main/java/com/fletmanagement/demo/service/LastLocationServiceImp.java
+++ src/main/java/com/fletmanagement/demo/service/LastLocationServiceImp.java
@@ -3,10 +3,13 @@ package com.fletmanagement.demo.service;
 import com.fletmanagement.demo.model.Trajectories;
 import com.fletmanagement.demo.repository.TrajectoriesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 
 @Service
@@ -22,6 +25,7 @@ public class LastLocationServiceImp implements LastLocationService {
     @Override
     @Transactional
     public Page<Trajectories> findLatestTaxiLocations(Pageable pageable) {
-        return trajectoriesRepository.findLatestTaxiLocations(pageable);
+        List<Trajectories> trajectories = trajectoriesRepository.findLatestTaxiLocations(pageable);
+        return new PageImpl<>(trajectories,pageable, trajectories.size());
     }
 }
